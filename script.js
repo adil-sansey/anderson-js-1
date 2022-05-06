1768a289d66f288d1393a7cb60be0a8c659e362d
adil-sansey <adil.sansey@gmail.com>
2022-05-06 18:10:49 +0600

    refactor: delete README.md

--- script.js
+++ script.js
@@ -3,31 +3,25 @@
 Array.prototype.myFilter = function (callback, thisArg = undefined) {
   const result = [];
 
-  for (let index = 0; index < this.length; index++) {
-    const elem = this[index];
-    const callbackResult = callback.call(thisArg, elem, index, this);
+  this.forEach((elem, index, array) => {
+    const callbackResult = callback.call(thisArg, elem, index, array);
 
     if (callbackResult) {
       result.push(elem);
     }
-  }
+  });
 
   return result;
 };
 
 function createDebounceFunction(callback, delay) {
   let timerId;
-  let start;
 
   return function () {
     if (timerId !== undefined) {
       clearTimeout(timerId);
     }
 
-    timerId = setTimeout(callback, delay, start);
-    if (start === undefined) {
-      start = Date.now();
-    }
-    console.log(timerId);
+    timerId = setTimeout(callback, delay);
   };
 }
